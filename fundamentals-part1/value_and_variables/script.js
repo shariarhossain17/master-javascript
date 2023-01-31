a23c291f9bc09058b83bd343df5ac18d7f8fb838
shariar17e <shariarhossain23@gmail.com>
2023-01-31 15:56:55 +0600

    value and variables

---
+++ fundamentals-part1/value_and_variables/script.js
@@ -0,0 +1,22 @@
+//  what is value ??
+// ans : value is  data
+/* example data */ console.log("shariar hossain"); // we use console.log to see the output
+
+// what is variable?
+// ans: variable is a box.that's use to store value. Imagine you have chocolate box this is the variable. in this chocolate box have chocolate this is the value.
+
+// variable convention
+
+// which convention  should use??
+/* Ans: Javascript variable convention is camelCase  like this */ let firstName =
+  "shariar";/* most of the developer use this convention */ 
+  /* moreover we use uppercase like*/ let FIRSTNAME ="shariar";
+/* lowercase like */ let firstname = "shariar";
+/* moreover use */ let first_name ="sharirar"; /* this convention. this convention most popular in other programming language like ruby */
+
+
+// Which convention should not be used in javascript??
+
+/* in javascript we should not be used javascript reserved keyword like */ let function = "something" /* it'is thorough syntax error. javascript has more reserved word  follow this link for reserved word https://www.w3schools.com/js/js_reserved.asp. but if you use this reserved word as a variable you can use underscore  (_) or dollar sign ($) like this */ ; let _function = "something"; let $function = "something"; /* you can't start variable with number like this */ let 3years = "something"; /* when you start variable with number it's through syntax error */ 
+
+
